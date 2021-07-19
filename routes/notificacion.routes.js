a3da5c7c20f87d35d0ff3a41de2973cce649ec4a
Diego Morejon <diegomorejonyanez@gmail.com>
2021-07-19 17:19:26 -0500

    prueba de conf de error de ruta

--- routes/notificacion.routes.js
+++ routes/notificacion.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 const router = express.Router();
 const { authJwt } = require("../middlewares");
 const { verifySignUp } = require("../middlewares");
-const Controller = require("../controllers/Notificacion.controller.js");
+const Controller = require("../controllers/notificacion.controller.js");
 const upload = require('../libs/storage');
 const cpUpload = upload.fields([{ name: 'filename', maxCount: 1 }])
 
